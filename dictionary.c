0ad6d32c92236257df91c4c44f34218d904a35b6
Md Hasib <md.hasibuzzaman001@google.com>
2022-04-06 18:48:46 +0600

    Added some comment for understand the code

--- dictionary.c
+++ dictionary.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(){
     char dictionary [10][2][15]={
+        //Vocabolary and words
         "Good","Goed",
         "Bad", "Slecht",
         "Man","Man",
@@ -14,15 +16,21 @@ int main(){
         "Hen","Kip"
     };
     char english[15];
+
+    //Taking Input of English word
     printf("Enter an English Word:");
     gets(english);
     int i,c=0;
     for(i=0;i<10;i++){
         if(strcmp(english,dictionary[i][0])==0){
+
+            //Print of the Dutch Meaning
             printf("Dutch Meaning is: %s\n",dictionary[i][1]);
         }
         else c++;
     }
+
+    //If didn't match error message
     if(c==10)printf("The word is not into my dictionary!");
     return 0;
 
